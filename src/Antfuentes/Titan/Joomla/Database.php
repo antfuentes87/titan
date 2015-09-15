1e956e4c46872a4b8cb1b70f7055320d1b6b69bb
antfuentes87 <antfuentes87@gmail.com>
2015-09-15 15:35:55 -0500

    more spacing test

--- src/Antfuentes/Titan/Joomla/Database.php
+++ src/Antfuentes/Titan/Joomla/Database.php
@@ -25,9 +25,10 @@ class Database{
             mysqli_set_charset($this->link, 'utf8');
         }
     }
-function __destruct() {
-mysqli_close($this->link);
-}
+
+	function __destruct() {
+		mysqli_close($this->link);
+	}
 	
 	public function q($q){
 		$data = array();
