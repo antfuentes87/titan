278fdc945be2cc09daedb0dfc755fe7ccbbf8787
antfuentes87 <antfuentes87@gmail.com>
2015-09-15 15:51:43 -0500

    final render of spaces / tabs

--- src/Antfuentes/Titan/Joomla/Database.php
+++ src/Antfuentes/Titan/Joomla/Database.php
@@ -14,26 +14,26 @@ use Antfuentes\Titan\Framework;
 */
 
 class Database{
-    public $link;
+	public $link;
+	
+	function __construct(){
+		$this->link = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+		if (mysqli_connect_errno()){
+			printf("Connect failed: %s\n", mysqli_connect_error());
+			exit();
+		}else{
+			mysqli_set_charset($this->link, 'utf8');
+		}
+	}
 
-    function __construct(){
-    	$this->link = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-    	if (mysqli_connect_errno()) {
-    		printf("Connect failed: %s\n", mysqli_connect_error());
-    		exit();
-        }else{
-            mysqli_set_charset($this->link, 'utf8');
-        }
-    }
-
-	function __destruct() {
+	function __destruct(){
 		mysqli_close($this->link);
 	}
-	
+
 	public function q($q){
 		$data = array();
-    	$result = mysqli_query($this->link, $q);
-    	if($result){
+		$result = mysqli_query($this->link, $q);
+		if($result){
 			while($row = mysqli_fetch_assoc($result)){
 				$data[] = $row;
 			}
@@ -46,19 +46,19 @@ class Database{
 		foreach($array as $key => $val){	
 			$table = explode('_', $val['Tables_in_'.DB_NAME]);
 			$varname = $table[1];
-     		$this->$varname = $table[0].'_'.$table[1];
+			$this->$varname = $table[0].'_'.$table[1];
 		}
-    }
-    
+	}
+
 	public function columns($table){
-    	$array = $this->q('SHOW COLUMNS FROM '. $table);
-    	var_dump($array);
+		$array = $this->q('SHOW COLUMNS FROM '. $table);
+		var_dump($array);
 		foreach($array as $key => $val){
 			$varname = $val['Field'];
-     		$this->$varname = $val['Field'];
+			$this->$varname = $val['Field'];
 		}
-    }
-	
+	}
+
 	public function variables($results){
 		foreach($results as $resultKey => $result){
 			foreach ($result as $column => $data) {
@@ -73,11 +73,11 @@ class Database{
 		}
 	}
 
-    public function dump($dump){
-        $h = new Framework\Html();
-        $h->b('pre', 0, 1);
-            var_dump($dump);
-        $h->b('pre', 1, 1);
-    }
+	public function dump($dump){
+		$h = new Framework\Html();
+		$h->b('pre', 0, 1);
+			var_dump($dump);
+		$h->b('pre', 1, 1);
+	}
 }
 ?>
